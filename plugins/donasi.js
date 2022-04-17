12ecc98b5733e75c0aff582b3e0e2b4c866c72be
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-04-17 13:04:06 +0530

    Add files via upload

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,5 @@
 let handler =  m => m.reply(` ❰𝐀𝐫𝐜𝐭𝐢𝐱 × 𝐀𝐫𝐜𝐞𝐮𝐬 𝐢𝐧𝐜.❱
-https://chat.whatsapp.com/ICQEbIj8PUj3bkPdgmULbN
+https://chat.whatsapp.com/Kb6msWfU8fP25KcG3M4Xtw
 `.trim()) // Add yourself if you want
 handler.help = ['donate']
 handler.tags = ['info']
