fa1139bd5013534e743b034a0f965759a46ce3b5
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-04-23 21:39:39 +0530

    Add files via upload

--- plugins/nulis=textpro.js
+++ plugins/nulis=textpro.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*List Effect*\n\nEXAMPLE:\n*.textpro 1917 zabot|zabot*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  if (!effect) throw '*List Effect*\n\nEXAMPLE:\n*.textpro/style 1917 zabot|zabot*\n\n' + effects.sort((a, b) => a + b).join('\n 🔰 ')
   effect = effect.toLowerCase()
   if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
@@ -10,8 +10,8 @@ let handler = async (m, { conn, args: [effect], text: txt }) => {
   let url = global.API('xteam', '/textpro/' + effect, { text, text2 }, 'APIKEY')
   await conn.sendFile(m.chat, url, 'textpro.jpg', `*TEXTPRO*\n*Effect:* ${effect}`, m)
 }
-handler.help = ['textpro'].map(v => v + '')
+handler.help = ['textpro','style'].map(v => v + '')
 handler.tags = ['nulis']
-handler.command = /^(textpro)$/i
+handler.command = /^(textpro|style)$/i
 
 export default handler
