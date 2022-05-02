00fb8c02141ac395182e08fba796bb320b1684d1
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-05-03 03:09:34 +0530

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -1,19 +1,21 @@
-const { mediafireDl } = require('../lib/mediafire.js')
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-if (!text) return m.reply(`Kirim perintah ${usedPrefix + command} *link mediafire*`)
-if (!args[0].includes('mediafire.com')) return m.reply(error.linkmf)
-let mdjon = args.join(' ')
-res = await mediafireDl(mdjon)
-result = `「 *MEDIAFIRE DOWNLOAD* 」
-*Data Berhasil Didapatkan!*
-🆔 Nama : ${res[0].nama}
-📊 Ukuran : ${res[0].size}
-💬 Link : ${res[0].link}
-_Tunggu Proses Upload Media_`
-m.reply(result)
-//await sleep(100)
-conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
+import { mediafiredl } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
+    let res = await mediafiredl(args[0])
+    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
+    let caption = `
+*Name:* ${filename}
+*Size:* ${filesizeH}
+*Extension:* ${ext}
+*Uploaded:* ${aploud}
+`.trim()
+    m.reply(caption)
+    await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
 }
-handler.command = ['mediafire']
+handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(mediafire|mf)$/i
 
-module.exports = handler
+handler.limit = true
+
+export default handler
