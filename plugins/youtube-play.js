44fa7d0932086bd078124374b7e0cd68f1ffbe65
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-05-03 15:47:12 +0530

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -3,16 +3,20 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
   let vid = (await youtubeSearch(text)).video[0]
   if (!vid) throw 'Video/Audio Not found'
-  let { title, description, thumbnail, videoId, durationH } = vid
+  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
   const url = 'https://www.youtube.com/watch?v=' + videoId
   await conn.sendHydrated(m.chat, `
 🌎 *𝗧𝗜𝗧𝗟𝗘:* ${title}
 🌍 *𝗨𝗥𝗟:* ${url}
 🌞 *𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗧𝗜𝗢𝗡:* ${description}
-  `.trim(), author, thumbnail, '', '', null, null, [
+⏲️ *Published:* ${publishedTime}
+⌚ *Duration:* ${durationH}
+👁️ *Views:* ${viewH}
+  `.trim(), author, thumbnail, url, 'Go to YouTube', null, null, [
     ['SONG', `${usedPrefix}yta ${url} yes`],
-    ['VIDEO', `${usedPrefix}ytv ${url} yes`]
-  ], m, { asLocation: 1 })
+    ['VIDEO', `${usedPrefix}ytv ${url} yes`],
+    ['Youtube Search🔎', `${usedPrefix}yts ${url}`]
+  ], m)
 }
 handler.help = ['play', 'play2'].map(v => v + '')
 handler.tags = ['downloader']
