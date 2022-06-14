c9d4910c6a2257dc56a73e4bea8c2d1a5c25f82b
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-06-14 09:19:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['447451240296','Arctix',true],
+  ['447451239331','Arctix',true],
   ['447451231393','Arceus',true],
   ['919337956084', 'sibu', true],
   //['447451240296']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = ['919337956084,447451240296,447451231393'] // Premium user has unlimited limit
+global.prems = ['919337956084,447451239331,447451231393'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
